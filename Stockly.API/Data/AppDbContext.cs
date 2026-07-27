d752d8e021ffc38d65ba8db478da29b369fa8aae
Ahmed Farouk <a7medsoltan2004@gmail.com>
2026-07-27 16:45:13 +0300

    Fixed a dependency bug in AppDbContext

--- Stockly.API/Data/AppDbContext.cs
+++ Stockly.API/Data/AppDbContext.cs
@@ -1,5 +1,7 @@
 namespace Stockly.API.Data;
 using Microsoft.EntityFrameworkCore;
+using Stockly.API.Models;
+
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
