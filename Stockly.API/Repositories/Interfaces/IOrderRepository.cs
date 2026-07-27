110fd7bac969b06b6be288b5a04677e22b3f8977
Ahmed Farouk <a7medsoltan2004@gmail.com>
2026-07-27 16:45:54 +0300

    defined interfaces for repositories.

---
+++ Stockly.API/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Stockly.API.Models;
+
+namespace Stockly.API.Repositories.Interfaces
+{
+    public interface IOrderRepository 
+    {
+        Task<IEnumerable<Order>> GetAllAsync();
+
+        Task<Order?> GetByIdAsync(int id);
+
+        Task<Order> CreateAsync(Order order);
+    }
+}
---
+++ Stockly.API/Repositories/Interfaces/IProductRepository.cs.cs
@@ -0,0 +1,19 @@
+﻿using Stockly.API.Models;
+
+namespace Stockly.API.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+
+        Task<Product?> GetByIdAsync(int id);
+
+        Task<IEnumerable<Product>> SearchByNameAsync(string name);
+
+        Task<Product> CreateAsync(Product product);
+
+        Task<Product?> UpdateAsync(Product product);
+
+        Task DeleteAsync(int id);
+    }
+}
