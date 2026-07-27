95ccd1bf58511cce68d0b9123411036a081674a1
Ahmed Farouk <a7medsoltan2004@gmail.com>
2026-07-27 16:46:11 +0300

    Implemented Repositories.

---
+++ Stockly.API/Repositories/OrderRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stockly.API.Data;
+using Stockly.API.Models;
+using Stockly.API.Repositories.Interfaces;
+
+
+namespace Stockly.API.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext _context;
+
+        public OrderRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Order> CreateAsync(Order order)
+        {
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+            return order;
+        }
+
+        public async Task<IEnumerable<Order>> GetAllAsync()
+        {
+            return await _context.Orders
+                .Include(order => order.OrderItems)
+                .ThenInclude(orderitem => orderitem.Product)
+                .ToListAsync();
+        }
+
+        public async Task<Order?> GetByIdAsync(int id)
+        {
+            return await _context.Orders
+                .Include(orders => orders.OrderItems)
+                .ThenInclude(orderitem => orderitem.Product)
+                .FirstOrDefaultAsync(order => order.Id == id);
+        }
+    }
+}
---
+++ Stockly.API/Repositories/ProductRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stockly.API.Data;
+using Stockly.API.Models;
+using Stockly.API.Repositories.Interfaces;
+
+namespace Stockly.API.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly AppDbContext _context;
+
+    public ProductRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Product> CreateAsync(Product product)
+    {
+        await _context.Products.AddAsync(product);
+        await _context.SaveChangesAsync();
+
+        return product;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product == null)
+        {
+            return;
+        }
+
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Product>> GetAllAsync()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    public async Task<Product?> GetByIdAsync(int id)
+    {
+        return await _context.Products.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Product>> SearchByNameAsync(string name)
+    {
+        return await _context.Products.Where(product => product.Name.Contains(name)).ToListAsync();
+    }
+
+    public async Task<Product?> UpdateAsync(Product product)
+    {
+        var existingProduct = await _context.Products.FindAsync(product.Id);
+
+        if (existingProduct == null)
+        {
+            return null;
+        }
+
+        existingProduct.Name = product.Name;
+        existingProduct.Description = product.Description;
+        existingProduct.Price = product.Price;
+        existingProduct.StockQuantity = product.StockQuantity;
+
+        await _context.SaveChangesAsync();
+
+        return existingProduct;
+    }
+}
