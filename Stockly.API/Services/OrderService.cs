f01513f0ad1dd10934100f56a91f4f4ebb258da8
Ahmed Farouk <a7medsoltan2004@gmail.com>
2026-07-28 17:26:12 +0300

    Implemented Product and Order Services

---
+++ Stockly.API/Services/OrderService.cs
@@ -0,0 +1,107 @@
+﻿using Stockly.API.DTOs;
+using Stockly.API.Repositories.Interfaces;
+using Stockly.API.Services.Interfaces;
+using Stockly.API.Models;
+using System.Runtime.CompilerServices;
+
+namespace Stockly.API.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IProductRepository _productRepository;
+
+        public OrderService(IOrderRepository orderRepository, IProductRepository productRepository)
+        {
+            _orderRepository = orderRepository;
+            _productRepository = productRepository;
+        }
+
+        public async Task<OrderDto?> CreateOrderAsync(CreateOrderDto dto)
+        {
+            decimal totalAmount = 0;
+
+            Order order = new();
+            order.OrderDate = DateTime.Now;
+
+            foreach (var item in dto.Items)
+            {
+                var product = await _productRepository.GetByIdAsync(item.ProductId);
+
+                if (product == null)
+                {
+                    return null;
+                }
+
+                if (product.StockQuantity < item.Quantity)
+                {
+                    return null;
+                }
+
+                product.StockQuantity -= item.Quantity;
+
+                await _productRepository.UpdateAsync(product);
+
+                totalAmount += product.Price * item.Quantity;
+
+                OrderItem orderItem = new();
+                orderItem.ProductId = item.ProductId;
+                orderItem.Quantity = item.Quantity;
+                orderItem.UnitPrice = product.Price;
+
+                order.OrderItems.Add(orderItem);
+            }
+
+            order.TotalAmount = totalAmount;
+            var createdOrder = await _orderRepository.CreateAsync(order);
+            OrderDto orderDto = new();
+            orderDto.Id = createdOrder.Id;
+            orderDto.OrderDate = createdOrder.OrderDate;
+            orderDto.TotalAmount = createdOrder.TotalAmount;
+            return orderDto;
+        }
+
+        public async Task<IEnumerable<OrderDto>> GetAllOrdersAsync()
+        {
+            IEnumerable<Order> orders = await _orderRepository.GetAllAsync();
+            List<OrderDto> orderDtos = new ();
+            foreach (var order in orders)
+            {
+                orderDtos.Add(new OrderDto 
+                {
+                    Id = order.Id,
+                    OrderDate = order.OrderDate,
+                    TotalAmount = order.TotalAmount,
+                });
+
+            }
+            return orderDtos;
+        }
+
+        public async Task<OrderDto?> GetOrderByIdAsync(int id)
+        {
+            var order = await _orderRepository.GetByIdAsync(id);
+            if(order == null)
+            {
+                return null;
+            }
+            OrderDto orderDto = new();
+            OrderItemDto itemDto = new();
+            orderDto.Id = order.Id;
+            orderDto.OrderDate = order.OrderDate;
+            orderDto.TotalAmount = order.TotalAmount;
+            foreach (var item in order.OrderItems)
+            {
+                OrderItemDto orderItemDto = new();
+
+                orderItemDto.ProductId = item.ProductId;
+                orderItemDto.ProductName = item.Product.Name;
+                orderItemDto.UnitPrice = item.UnitPrice;
+                orderItemDto.Quantity = item.Quantity;
+
+                orderDto.Items.Add(orderItemDto);
+            }
+            return orderDto;
+        }
+    }
+}
---
+++ Stockly.API/Services/ProductService.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stockly.API.DTOs;
+using Stockly.API.Models;
+using Stockly.API.Repositories.Interfaces;
+using Stockly.API.Services.Interfaces;
+
+namespace Stockly.API.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        public async Task<ProductDto> CreateProductAsync(CreateProductDto dto)
+        {
+            Product product = new();
+            product.Name = dto.Name;
+            product.Description = dto.Description;
+            product.Price = dto.Price;
+            product.StockQuantity = dto.StockQuantity;
+
+            Product createdProduct = await _productRepository.CreateAsync(product);
+
+            ProductDto productDto = new();
+            productDto.Id = createdProduct.Id;
+            productDto.Name = createdProduct.Name;
+            productDto.Description = createdProduct.Description;
+            productDto.Price = createdProduct.Price;
+            productDto.StockQuantity = createdProduct.StockQuantity;
+
+            return productDto;
+            
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            await _productRepository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetAllProductsAsync()
+        {
+            IEnumerable<Product> products = await _productRepository.GetAllAsync();
+            List<ProductDto> productDtos = new ();
+            foreach (var product in products)
+            {
+                productDtos.Add(new ProductDto
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Description = product.Description,
+                    Price = product.Price,
+                    StockQuantity = product.StockQuantity
+                });
+            }
+            return productDtos;
+        }
+
+        public async Task<ProductDto?> GetProductByIdAsync(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return null;
+            }
+            ProductDto productdto = new();
+            productdto.Id = product.Id;
+            productdto.Name = product.Name;
+            productdto.Description = product.Description;
+            productdto.Price = product.Price;
+            productdto.StockQuantity = product.StockQuantity;
+            return productdto;
+        }
+
+        public async Task<IEnumerable<ProductDto>> SearchProductsByNameAsync(string name)
+        {
+            var products = await _productRepository.SearchByNameAsync(name);
+            List<ProductDto> productDtos = new ();
+            foreach(var product in products)
+            {
+                productDtos.Add(new ProductDto
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Description = product.Description,
+                    Price = product.Price,
+                    StockQuantity = product.StockQuantity
+                });
+            }
+            return productDtos;
+            
+        }
+
+        public async Task<ProductDto?> UpdateProductAsync(int id, UpdateProductDto dto)
+        {
+            Product product = new();
+            product.Id = id;
+            product.Name = dto.Name;
+            product.Description = dto.Description;
+            product.Price = dto.Price;
+            product.StockQuantity = dto.StockQuantity;
+            var updatedProduct = await _productRepository.UpdateAsync(product);
+            if (updatedProduct == null)
+            {
+                return null;
+            }
+            ProductDto updatedProductDto = new();
+            updatedProductDto.Id = updatedProduct.Id;
+            updatedProductDto.Name = updatedProduct.Name;
+            updatedProductDto.Description = updatedProduct.Description;
+            updatedProductDto.Price = updatedProduct.Price;
+            updatedProductDto.StockQuantity = updatedProduct.StockQuantity;
+            return updatedProductDto;
+        }
+    }
+}
