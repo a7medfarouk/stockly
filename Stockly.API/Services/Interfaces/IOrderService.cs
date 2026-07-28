f88e329d7ece174c5b96e6d333a64adf76ace0d1
Ahmed Farouk <a7medsoltan2004@gmail.com>
2026-07-28 17:25:50 +0300

    Defined Service Interfaces

---
+++ Stockly.API/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using Stockly.API.DTOs;
+
+namespace Stockly.API.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<OrderDto>> GetAllOrdersAsync();
+        Task<OrderDto?> GetOrderByIdAsync(int id);
+        Task<OrderDto?> CreateOrderAsync(CreateOrderDto dto);
+    }
+}
---
+++ Stockly.API/Services/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Stockly.API.DTOs;
+
+namespace Stockly.API.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetAllProductsAsync();
+        Task<ProductDto?> GetProductByIdAsync(int id);
+        Task<IEnumerable<ProductDto>> SearchProductsByNameAsync(string name);
+        Task<ProductDto> CreateProductAsync(CreateProductDto dto);
+        Task<ProductDto?> UpdateProductAsync(int id, UpdateProductDto dto);
+        Task DeleteProductAsync(int id);
+    }
+}
