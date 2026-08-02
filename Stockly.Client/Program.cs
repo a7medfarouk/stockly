f1533faa5dcb469426755f8b83909995bbf6de75
Ahmed Farouk <a7medsoltan2004@gmail.com>
2026-08-02 15:52:21 +0300

    registered client services with dependency injection

--- Stockly.Client/Program.cs
+++ Stockly.Client/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Stockly.Client;
+using Stockly.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5237") });
+builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<OrderService>();
 
 await builder.Build().RunAsync();
