ce0c8e8591c30882356bb81c230e35212ce35145
Ahmed Farouk <a7medsoltan2004@gmail.com>
2026-08-03 11:45:05 +0300

    new migrations after adding the new isdeleted column to products

--- Stockly.API/Migrations/20260803084327_AddSoftDeleteToProducts.Designer.cs
+++ Stockly.API/Migrations/20260803084327_AddSoftDeleteToProducts.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Stockly.API.Data;
 
@@ -11,9 +12,11 @@ using Stockly.API.Data;
 namespace Stockly.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260803084327_AddSoftDeleteToProducts")]
+    partial class AddSoftDeleteToProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
